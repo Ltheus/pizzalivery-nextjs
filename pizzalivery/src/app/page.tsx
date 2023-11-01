9c9eb86ccd3d72d7cb26469f2365b9b55119272c
Ltheus <matheusglopes1@gmail.com>
2023-11-01 20:10:13 -0300

    TEMOS FOOTER

--- pizzalivery/src/app/page.tsx
+++ pizzalivery/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import { Logo } from '@/components/logo/Logo'
+import { Footer } from '@/components/footer/Footer'
 
 export default function Home() {
   return (
     <main>
-      <Logo></Logo>
+      {/* <Logo></Logo> */}
+      <Footer></Footer>
     </main>
   )
 }
