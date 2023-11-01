13c8141d435115b2d35032c4e84b9e5ea7000809
Ltheus <matheusglopes1@gmail.com>
2023-11-01 19:57:29 -0300

    TEMOS LOGO

--- pizzalivery/src/app/page.tsx
+++ pizzalivery/src/app/page.tsx
@@ -1,13 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { Logo } from '@/components/logo/Logo'
 
 export default function Home() {
   return (
     <main>
-      <div>
-        <h1>Pizzalivery</h1>
-        <p>Iniciar pedido</p>
-      </div>
+      <Logo></Logo>
     </main>
   )
 }
