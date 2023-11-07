b9b4d04c05d73a8617785e86655b32560ca77069
Ltheus <matheusglopes1@gmail.com>
2023-11-07 11:42:06 -0300

    Upload pizza images + update flavours page (WIP)

--- pizzalivery/src/components/button/Button.tsx
+++ pizzalivery/src/components/button/Button.tsx
@@ -5,6 +5,7 @@ import styled from "./button.module.css";
 interface ButtonProps {
   children: React.ReactNode;
   onClick: React.ReactEventHandler;
+  id?: string
 }
 
 export const Button = ({ children, onClick, ...rest }: ButtonProps) => {
