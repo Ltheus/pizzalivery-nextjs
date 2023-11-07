ffc73a86e348273d2c1a0fbffc2d5e6e6286e4b8
Ltheus <matheusglopes1@gmail.com>
2023-11-06 22:00:29 -0300

    Swith static sizes to size variables  (now working)

--- pizzalivery/src/app/page.tsx
+++ pizzalivery/src/app/page.tsx
@@ -1,14 +1,20 @@
 "use client"
 
+import { useRouter } from 'next/navigation'
 import styled from './page.module.css'
 import { Button } from '@/components/button/Button'
 
 // ---------------------- Home Page ----------------------
 
 export default function Home() {
+  const router = useRouter()
+  const handleStartOrder = () => {
+    router.push('./sizes')
+  }
+
   return (
     <div className={styled.homeWrapper}>
-      <Button onClick={() => {}}> Iniciar Pedido </Button>
+      <Button onClick={handleStartOrder}> Iniciar Pedido </Button>
     </div>
-  )
+  );
 }
