1283f2f172a827b3a3052aa4cad1261d9b68c334
Ltheus <matheusglopes1@gmail.com>
2023-11-08 09:39:16 -0300

    Added login route

--- pizzalivery/src/components/header/Header.tsx
+++ pizzalivery/src/components/header/Header.tsx
@@ -1,15 +1,18 @@
 "use client"
 
+import { useRouter } from "next/navigation"
 import { Button } from "../button/Button"
 import { Logo } from "../logo/Logo"
 import styled from "./header.module.css"
 
 export const Header = () => {
+    const router = useRouter()
+
     return(
         <header className={styled.header}>
             <div className={styled.headerDiv}>
                 <Logo></Logo>
-                <Button onClick={() => {}}> Login </Button>
+                <Button onClick={() => {router.push('/login')}}> Login </Button>
             </div>
         </header>
     )
