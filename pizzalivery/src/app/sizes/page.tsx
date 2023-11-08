576b124bc62a4db3e52d6d423a251a108e106358
Ltheus <matheusglopes1@gmail.com>
2023-11-08 19:30:39 -0300

    API fetch on /sizes

--- pizzalivery/src/app/sizes/page.tsx
+++ pizzalivery/src/app/sizes/page.tsx
@@ -1,93 +1,70 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/button/Button";
-import styled from "./sizes.module.css"
-import { Title } from "@/components/title/Title"
+import styled from "./sizes.module.css";
+import { Title } from "@/components/title/Title";
 import { useRouter } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
 import OrderContext from "../contexts/OrderContext";
 
 export default function Sizes() {
-  
-  const router = useRouter()
+  const router = useRouter();
   const { pizzaSize, setPizzaSize } = useContext(OrderContext);
+  const [sizeId, setSizeId] = useState("");
+  const [pizzaSizeOptions, setPizzaSizeOptions] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const sizeOptions = [
-      {
-        id: "10",
-        flavours: 1,
-        size: 35,
-        slices: 8,
-        text: "Grande",
-      },
-      {
-        id: "11",
-        flavours: 2,
-        size: 35,
-        slices: 8,
-        text: "Grande",
-      },
-      {
-        id: "20",
-        flavours: 1,
-        size: 28,
-        slices: 4,
-        text: "Média",
-      },
-      {
-        id: "21",
-        flavours: 2,
-        size: 28,
-        slices: 4,
-        text: "Média",
-      },
-      {
-        id: "30",
-        flavours: 1,
-        size: 18,
-        slices: 1,
-        text: "Broto",
-      },
-      {
-        id: "31",
-        flavours: 2,
-        size: 18,
-        slices: 1,
-        text: "Broto",
-      },
-    ];
-    
-    const [sizeId, setSizeId] = useState("")
+  const getPizzaSizeOptions = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch("http://localhost:8000/pizza/sizes");
+      const options = await response.json();
+      setPizzaSizeOptions(options);
+    } catch (error) {
+      alert(`Não foi possível acessar a API - ${error}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    const getPizzaSize = (id: string) => {
-      return sizeOptions.filter((option) => option.id === id);
-    };
+  const getPizzaSize = (id: string) => {
+    return pizzaSizeOptions.filter((option) => option.id === id);
+  };
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setSizeId(event.target.value);
-    };
-    
-    const handleBack = () => {
-      router.push('/')
-    };
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSizeId(event.target.value);
+  };
 
-    const handleNext = () => {
-      const selectedSize = getPizzaSize(sizeId);
-      setPizzaSize(selectedSize);
-      router.push('/flavours')
-    };
+  const handleBack = () => {
+    router.push("/");
+  };
 
-    useEffect(() => {
-      if (!pizzaSize) return;
+  const handleNext = () => {
+    const selectedSize = getPizzaSize(sizeId);
+    setPizzaSize(selectedSize);
+    router.push("/flavours");
+  };
 
-      setSizeId(pizzaSize[0].id);
-    }, []);
+  useEffect(() => {
+    if (!pizzaSize) return;
 
-    return (
-      <>
-        <Title tabIndex={0}> Escolha o tamanho da sua pizza </Title>
+    setSizeId(pizzaSize[0].id);
+  }, []);
+
+  useEffect(() => {
+    getPizzaSizeOptions();
+  }, []);
+
+  return (
+    <>
+      <Title tabIndex={0}> Escolha o tamanho da sua pizza </Title>
+      {isLoading ? (
         <section className={styled.sizeContentWrapper}>
-          {sizeOptions.map(({ id, size, slices, flavours, text }) => (
+          <Title>Carregando</Title>
+        </section>
+      ) : (
+        <section className={styled.sizeContentWrapper}>
+          {pizzaSizeOptions.map(({ id, size, slices, flavours, text }) => (
             <div className={styled.radioCard} key={id}>
               <input
                 type="radio"
@@ -106,10 +83,11 @@ export default function Sizes() {
             </div>
           ))}
         </section>
-        <div className={styled.sizeActionWrapper}> 
-          <Button onClick={handleBack}>Voltar</Button>
-          <Button onClick={handleNext}>Escolha o sabor</Button>
-        </div>
-      </>
-    );
+      )}
+      <div className={styled.sizeActionWrapper}>
+        <Button onClick={handleBack}>Voltar</Button>
+        <Button onClick={handleNext}>Escolha o sabor</Button>
+      </div>
+    </>
+  );
 }
